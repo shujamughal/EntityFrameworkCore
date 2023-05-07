74d54db33b6ac85c6622389bedca840f104d5fe7
Dr. Shuja ur Rehman Baig <shujamughal@gmail.com>
2023-05-07 18:36:43 +0500

    EF Core Database First Frist Article Code

---
+++ MyConsoleApplication/MyConsoleApplication/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Scaffold-DbContext 'Data Source=(localdb)\ProjectModels;Initial Catalog=MyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False' Microsoft.EntityFrameworkCore.SqlServer
